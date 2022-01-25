30632b876ca2538cbf5b68203154ef5df852de80
unknown <eyuptrabzonn@gmail.com>
2022-01-25 23:58:57 +0100

    Updated UserManager

--- Recognition/users/UserManager.py
+++ Recognition/users/UserManager.py
@@ -8,27 +8,32 @@ from Recognition.users.User import User
 class UserManager:
 
     def __init__(self):
-        self.__users = []
+        self.collection = ObjectsManager.DatabaseManager().getDatabaseService().getDatabase()['users_whitelisted']
+        self.__users = {}
         self.__registerUsers()
 
     def __registerUsers(self):
-        collection = ObjectsManager.DatabaseManager().getDatabaseService().getDatabase()['users_whitelisted']
-        cursor = collection.find({})
+        cursor = self.collection.find({})
         for records in cursor:
             id = str(records['id'])
             name = records['name']
             lastname = records['lastname']
             age = records['age']
             train_data = json.loads(str(records['train_data']))
-            self.__users.append(User(id,name,lastname,age,train_data))
+            self.__users[id] = User(id,name,lastname,age,train_data)
         print("\n [INFO] Users from the database are registered!")
 
     def getUsers(self):
         return self.__users
 
     def getUser(self,id):
-        for user in self.__users:
-            if int(user.getId()) == id:
-                return user
+        return self.__users.get(id)
+
+    def removeUser(self,id):
+        del self.__users[id]
+
+    def addUser(self, User):
+        self.__users[User.getId()] = User
+
 
 
