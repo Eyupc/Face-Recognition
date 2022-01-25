67240d9866b79282c0987898dce3e1cda870f638
unknown <eyuptrabzonn@gmail.com>
2022-01-25 23:59:38 +0100

    Small changes

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-import base64
-import io
-import threading
+
 import cv2
-import face_recognition
+
 from ObjectsManager import ObjectsManager
-from Recognition.FaceTrainerr import FaceTrainer
+from Recognition.FaceTrainer import FaceTrainer
+from Recognition.TrainerManager import TrainerManager
 
 obj = ObjectsManager()
 cam = cv2.VideoCapture(0)
 
+trainer = TrainerManager()
+trainer.train()
+#print(trainer.getData())
+exit(0)
 while True:
     ret,img = cam.read()
     k = cv2.waitKey(10) & 0xff
