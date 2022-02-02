b0d0db9682326705ef7aef1455901ce81b138855
MasterEyup <eyuptrabzonn@gmail.com>
2022-02-02 11:57:21 +0100

    update ws server

--- WS/WebSocketServer.py
+++ WS/WebSocketServer.py
@@ -1,39 +1,43 @@
 import asyncio
 import json
-import multiprocessing
+import threading
+from threading import Thread
 from asyncio import sleep
-from multiprocessing import Process
 
 import websockets
 
-class WebSocketServer(multiprocessing.Process):
+class WebSocketServer(Thread):
     def __init__(self, address, port):
-        super().__init__()
+        Thread.__init__(self)
         self.port = port
         self.address = address
-
     def run(self):
         loop = asyncio.new_event_loop()
-        ws_server = websockets.serve(self.ws_handler, self.address, self.port,
-                                     ping_timeout=None, ping_interval=None, loop=loop)
+        asyncio.set_event_loop(loop)
+        ws_server = websockets.serve(self.ws_handler, self.address, self.port)
         loop.run_until_complete(ws_server)
         loop.run_forever()
 
+
     async def ws_handler(self, websocket, path):
-        while True:
-            # simulate work
-            print("doing some work")
-            await sleep(5)
-            print("Sending data")
-            data = json.dumps({"test": "test test"})
-            try:
-                await websocket.send(data)
-                result = await websocket.recv()
-                print(result)
-                result = json.loads(result)
-                print(f"json: {result}")
-            except websockets.ConnectionClosed:
-                print(f"Terminated")
-                break
+        # simulate work
+        print("doing some work")
+        await sleep(5)
+        print("Sending data")
+        data = json.dumps({"test": "test test"})
+        try:
+            await websocket.send(data)
+            result = await websocket.recv()
+            print(result)
+            result = json.loads(result)
+            print(f"json: {result}")
+        except websockets.ConnectionClosed:
+            print(f"Terminated")
+            return
 
 ws = WebSocketServer("localhost",6969)
+thr = Thread(target=ws.run,daemon=True)
+thr.start()
+while True:
+    print('aaa')
+
