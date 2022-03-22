86af0782490ce6d8e23d32005d05438debebe024
unknown <eyuptrabzonn@gmail.com>
2022-03-22 22:42:44 +0100

    some changes, close window, stop threads on  close

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import threading
 import cv2
 import cv2.data
 import time
+import atexit
+
 from WS.WebSocketManager import WebSocketManager
 
 
@@ -22,7 +24,7 @@ class Main:
         try:
             self.recognizer.read('trainer.yml')
         except Exception:
-            print("Trainer.yml is empty!")
+            print("[INFO] Trainer.yml is empty!")
 
         self.cam = cv2.VideoCapture(0 + cv2.CAP_DSHOW)
         self.cam.set(3, 640)
@@ -32,14 +34,21 @@ class Main:
         self.font = cv2.FONT_HERSHEY_SIMPLEX
         self.startTime()
 
-        # self.bounding_box = {'top': 100, 'left': 1000, 'width': 900 , 'height': 540}
-
-        # self.sct = mss()
+        self.loop = True
 
     def startTime(self):
-        from ObjectsManager import ObjectsManager
-        collection = ObjectsManager.getDatabaseManager().getDatabaseService().getDatabase()['config']
+        collection = self.obj.getDatabaseManager().getDatabaseService().getDatabase()['stats']
         collection.update_one({}, {'$set': {'start_time': time.time()}})
+
+    def stop(self):
+        collection = self.obj.getDatabaseManager().getDatabaseService().getDatabase()['stats']
+        collection.update_one({}, {'$set': {'start_time': 0}})
+
+        print("[INFO] Closing the program..")
+        self.WebSocketServer.stop()
+        self.loop = False
+        self.cam.release()
+        raise SystemExit()
 
     def updateReader(self):
         self.recognizer = None
@@ -47,63 +56,68 @@ class Main:
         self.recognizer.read('trainer.yml')
 
     def run(self):
-        while True:
-            ret, img = self.cam.read()
-            # img = np.array(self.sct.grab(self.bounding_box))
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            faces = cv2.CascadeClassifier(
-                cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(
-                gray,
-                scaleFactor=1.2,
-                minNeighbors=5,
-                minSize=(int(self.minW), int(self.minH)),
-            )
+        while self.loop:
             try:
-                for (x, y, w, h) in faces:
-                    cv2.rectangle(img, (x, y), (x + w, y + h), (0,0,255), 2)
-                    if (len(self.obj.getUserManager().getUsers()) > 0):
-                        id, confidence = -1, 200
-                        try:
-                            id, confidence = self.recognizer.predict(gray[y:y + h, x:x + w])
-                        except Exception as e:
-                            print(e)
+                ret, img = self.cam.read()
+                # img = np.array(self.sct.grab(self.bounding_box))
+                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                faces = cv2.CascadeClassifier(
+                    cv2.data.haarcascades + 'haarcascade_frontalface_default.xml').detectMultiScale(
+                    gray,
+                    scaleFactor=1.2,
+                    minNeighbors=5,
+                    minSize=(int(self.minW), int(self.minH)),
+                )
+                try:
+                    for (x, y, w, h) in faces:
+                        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                        if (len(self.obj.getUserManager().getUsers()) > 0):
+                            id, confidence = -1, 200
+                            try:
+                                id, confidence = self.recognizer.predict(gray[y:y + h, x:x + w])
+                            except Exception as e:
+                                print("[ERROR] " + str(e))
 
-                        # print(id)
-                        #print(confidence)
-                        if confidence <= 90:
-                            # print("id: " + str(id))
-                            userInfo = self.obj.getUserManager().getUser(id)
-                            user = userInfo.getName() + " " + userInfo.getLastname() + " " + str(userInfo.getAge())
-                            confidence = "  {0}%".format(round(100 - confidence))
-                        else:
-                            user = "Unknown"
-                            confidence = "  {0}%".format(round(100 - confidence))
+                            # print(id)
+                            # print(confidence)
+                            if confidence <= 90:
+                                # print("id: " + str(id))
+                                userInfo = self.obj.getUserManager().getUser(id)
+                                user = userInfo.getName() + " " + userInfo.getLastname() + " " + str(userInfo.getAge())
+                                confidence = "  {0}%".format(round(100 - confidence))
+                            else:
+                                user = "Unknown"
+                                confidence = "  {0}%".format(round(100 - confidence))
 
-                        (ww, hh), _ = cv2.getTextSize(str(user), cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
-                        img = cv2.rectangle(img, (x, y), (x + ww, y - hh - 5), (0,0,255), -1)
-                        img = cv2.putText(img, str(user), (x, y - 5),
-                                          cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255,255,255), 2)
+                            (ww, hh), _ = cv2.getTextSize(str(user), cv2.FONT_HERSHEY_SIMPLEX, 0.6, 1)
+                            img = cv2.rectangle(img, (x, y), (x + ww, y - hh - 5), (0, 0, 255), -1)
+                            img = cv2.putText(img, str(user), (x, y - 5),
+                                              cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
-                        #cv2.putText(img, str(user), (x + 5, y - 5), self.font, 1, (255, 255, 255), 2)
-                        #cv2.putText(img, str(confidence), (x + 5, y + h - 5), self.font, 1, (255, 255, 0), 1)
+                            # cv2.putText(img, str(user), (x + 5, y - 5), self.font, 1, (255, 255, 255), 2)
+                            # cv2.putText(img, str(confidence), (x + 5, y + h - 5), self.font, 1, (255, 255, 0), 1)
+                except Exception as e:
+                    print("[INFO/ERROR] Updating Trainer.yml...\r\n Reason: " + str(e))
+                    pass
+
+                k = cv2.waitKey(10) & 0xff
+                if k == 27:
+                    raise KeyboardInterrupt
+
+                cv2.imshow('video', img)
+
+                base64_str = str(base64.b64encode(cv2.imencode('.jpg', img)[1]).decode("utf-8"))
+                data = {
+                    "header": "StreamEvent",
+                    "data": [{
+                        "message": base64_str
+                    }]
+                }
+                self.WebSocketServer.ioloop.add_callback(WebSocketManager.sendBroadcast, data, "StreamPage")
+            except KeyboardInterrupt as e:
+                self.stop()
             except Exception as e:
-                print("[INFO/ERROR] Updating Trainer.yml...\r\n Reason: " + str(e))
-                pass
-
-            k = cv2.waitKey(10) & 0xff
-            if k == 27: pass
-            # Main.updateReader()
-            # break
-
-            cv2.imshow('video', img)
-            base64_str = str(base64.b64encode(cv2.imencode('.jpg', img)[1]).decode("utf-8"))
-            data = {
-                "header": "StreamEvent",
-                "data": [{
-                    "message": base64_str
-                }]
-            }
-            self.WebSocketServer.ioloop.add_callback(WebSocketManager.sendBroadcast, data, "StreamPage")
+                print("[ERROR]: " + str(e))
 
 
 if __name__ == "__main__":
