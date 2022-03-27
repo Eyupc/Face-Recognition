6a70176903e0c36e2ec9a84b117ff406cf5eed3e
unknown <eyuptrabzonn@gmail.com>
2022-03-27 20:13:47 +0200

    Added WebSocket client, send data to ESP WS Server

---
+++ WSClient/WebSocketClient.py
@@ -0,0 +1,46 @@
+import asyncio
+import json
+import threading
+import websocket
+
+import websocket
+class WebSocketClient(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self,name="WebSocketClient")
+
+
+    def run(self):
+        websocket.enableTrace(True)
+        self.websocket = websocket.WebSocketApp("ws://192.168.0.191:8888",
+                                                on_message=lambda ws, msg: self.on_message(ws, msg),
+                                                on_error=lambda ws, msg: self.on_error(ws, msg),
+                                                on_close=lambda ws: self.on_close(ws),
+                                                on_open=lambda ws: self.on_open(ws))
+        self.ws = None
+        self.wst = threading.Thread(target=self.websocket.run_forever)
+        self.wst.daemon = True
+        self.wst.start()
+
+    def on_open(self,ws):
+        self.ws = ws
+
+    def on_message(self,ws,message):
+        pass
+
+    def on_error(self,ws,error):
+        pass
+
+    def on_close(self,ws):
+        pass
+
+    def sendMessage(self):
+        if(self.ws == None):
+            pass
+        data = {
+            "header": "OpenDoorEvent",
+            "data": [{
+                "open": True
+            }]
+        }
+        self.ws.send(json.dumps(data))
+
